73f240ca0e4808985a9f71350e1d701abb987aab
Alexandr Volodin <axtxxeffect85@gmail.com>
2024-03-18 14:20:13 +1000

    fix

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.96"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source = "yandex-cloud/yandex"
+#       version = "0.96"
+#     }
+#   }
+# }
 
 
 
